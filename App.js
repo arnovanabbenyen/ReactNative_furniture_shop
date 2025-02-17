c7bd93653fd40a326cef33902044e8a9cd41cd23
Arno Van Abbenyen <r0893222@student.thomasmore.be>
2025-02-17 15:13:21 +0100

    Opdracht componenten: productcard gelinkt + styling

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import ProductCard from "./components/ProductCard.js";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.container}>
+      <Text style={styles.title}>Our Collection</Text>
+      <ProductCard 
+        image={require("./Images/ode_chair.png")}
+        title="Ode Chair"
+        description="zwart, orion taupe12"
+        price= "399"
+      />
+      <ProductCard 
+        image={require("./Images/houten_kast.png")}
+        title="Modular Cabinet MC-5L"
+        description="Eiken hardwax olie naturel light 3041"
+        price="1759"
+      />
+      <ProductCard 
+        image={require("./Images/fauteuils.png")}
+        title="Co lounge chair"
+        description="Eiken gerookt gebeitst, orion turtle88"
+        price="889"
+      />
+      <ProductCard 
+        image={require("./Images/houten_tafel.png")}
+        title="Paste Eettafel"
+        description="Eiken naturel lak, Eiken naturel lak"
+        price="1909"
+      />
+      </View>
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexGrow: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingVertical: 20,
+    paddingTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
   },
 });
