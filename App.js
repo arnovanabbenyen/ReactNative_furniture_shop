6b8bde87f66e84118f27031ff28c8933503c7ecc
Arno Van Abbenyen <r0893222@student.thomasmore.be>
2025-02-24 18:23:47 +0100

    Opdracht navigatie: navigatiecontainer en stacknavigator zodat je kan navigeren tussen de schermen

--- App.js
+++ App.js
@@ -1,53 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
-import ProductCard from "./components/ProductCard.js";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen.js";
+import ProductDetail from "./screens/ProductDetail.js";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.container}>
-      <Text style={styles.title}>Our Collection</Text>
-      <ProductCard 
-        image={require("./Images/ode_chair.png")}
-        title="Ode Chair"
-        description="zwart, orion taupe12"
-        price= "399"
-      />
-      <ProductCard 
-        image={require("./Images/houten_kast.png")}
-        title="Modular Cabinet MC-5L"
-        description="Eiken hardwax olie naturel light 3041"
-        price="1759"
-      />
-      <ProductCard 
-        image={require("./Images/fauteuils.png")}
-        title="Co lounge chair"
-        description="Eiken gerookt gebeitst, orion turtle88"
-        price="889"
-      />
-      <ProductCard 
-        image={require("./Images/houten_tafel.png")}
-        title="Paste Eettafel"
-        description="Eiken naturel lak, Eiken naturel lak"
-        price="1909"
-      />
-      </View>
-    </ScrollView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={ProductDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    paddingVertical: 20,
-    paddingTop: 40,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 16,
-  },
-});
+}
