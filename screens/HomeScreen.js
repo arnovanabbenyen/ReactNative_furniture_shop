83d6c48a6b367affa979113fdb26f6e12877c508
Arno Van Abbenyen <r0893222@student.thomasmore.be>
2025-03-24 10:58:54 +0100

    Opdracht extra features: filtersysteem, zoekfunctie en sorteerfunctie toegevoegd

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,22 @@
 import React, {useEffect, useState} from "react";
-import { ScrollView, View, Text, StyleSheet } from "react-native";
+import { ScrollView, View, Text, StyleSheet, TextInput } from "react-native";
 import ProductCard from "../components/ProductCard.js";
+import { Picker } from "@react-native-picker/picker";
 
-import odeChairImage from "../Images/ode_chair.png";
-import houtenKastImage from "../Images/houten_kast.png";
-import fauteuilsImage from "../Images/fauteuils.png";
-import houtenTafelImage from "../Images/houten_tafel.png";
+const categoryNames = {
+  "" : "Alle categorieën",
+  "67d6ef65ac7150f21a8e5778" : "Salontafels",
+  "67d6eef832e54b05edf3f8fa" : "Fauteuils",
+  "67d6eeaddb995e343ac767ea" : "Wandkasten",
+  "67d6edc3fe53349ec5e0c44d" : "Eetkamerstoel",
+  "67d6ecffe994495169160be5" : "Eettafel",
+};
 
 const HomeScreen = ({ navigation }) => {
   const [products, setProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortOption, setSortOption] = useState("name");
 
   useEffect(() => {
     fetch(
@@ -21,41 +29,97 @@ const HomeScreen = ({ navigation }) => {
       }
     )
     .then((response) => response.json())
-    .then((data) =>
+    .then((data) => {
       setProducts(data.items.map((item) => ({
         id: item.product.id,
         title: item.product.fieldData.name,
         subtitle: item.product.fieldData.description,
         price: (item.skus[0]?.fieldData.price.value || 0) / 100,
         image: {uri: item.skus[0]?.fieldData["main-image"]?.url},
+        category: categoryNames[item.product.fieldData.category[0]] || "Onbekend",
       }))
     )
-  )
-    .catch((error) => console.error(error));
+  })
+    .catch(console.error);
   }, []);
 
+  const filteredProducts = products.filter(
+    (product) =>
+    (selectedCategory === "" || product.category === selectedCategory) &&
+    product.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const sortedProducts = [...filteredProducts].sort((a, b) => {
+    if (sortOption === "price-asc") return a.price - b.price;
+    if (sortOption === "price-desc") return b.price - a.price;
+    if (sortOption === "name-asc") return a.title.localeCompare(b.title);
+    if (sortOption === "name-desc") return b.title.localeCompare(a.title);
+  });
+
     return (
-        <ScrollView contentContainerStyle={styles.container}>
-          <View style={styles.container}>
-          <Text style={styles.heading}>Our Collection</Text>
-          {products.map((product) => (
+      <View style={styles.container}>
+        <Text style={styles.heading}>Our Collection</Text>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Zoek een model"
+          placeholderTextColor={"#000"}
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+        />
+        <View style={styles.pickerContainer}>
+          <Picker
+              selectedValue={selectedCategory}
+              onValueChange={setSelectedCategory}
+              style={styles.picker}
+            >
+            <Picker.item label="Alle categorieën" value="" />
+            {[...new Set(products.map((product) => product.category))].map((category) => (
+              <Picker.Item key={category} label={category} value={category} />
+            ))}
+          </Picker>
+        </View>
+        <View style={styles.pickerContainer}>
+          <Picker
+            selectedValue={sortOption}
+            onValueChange={setSortOption}
+            style={styles.picker}
+          >
+            <Picker.Item label="Prijs (laag - hoog" value="price-asc" />
+            <Picker.Item label="Prijs (hoog - laag)" value="price-desc" />
+            <Picker.Item label="Naam (A - Z)" value="name-asc" />
+            <Picker.Item label="Naam (Z - A)" value="name-desc" />
+          </Picker>
+        </View>
+        <ScrollView style={styles.cardContainer}>
+          <View style={styles.row}>
+          {filteredProducts.map((product) => (
           <ProductCard 
-            image={product.image}
-            title={product.title}
-            description={product.subtitle}
-            price={product.price}
+            key={product.id}
+            {...product}
             onPress={() => navigation.navigate("Details", product)}
           />
           ))}
           </View>
         </ScrollView>
+
+        <ScrollView style={styles.cardContainer}>
+          <View style={styles.row}>
+          {sortedProducts.map((product) => (
+          <ProductCard 
+            key={product.id}
+            {...product}
+            onPress={() => navigation.navigate("Details", product)}
+          />
+          ))}
+          </View>
+        </ScrollView>
+      </View>
       );
     };
     
     const styles = StyleSheet.create({
       container: {
         flexGrow: 1,
-        justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#fff',
         paddingVertical: 20,
@@ -66,11 +130,36 @@ const HomeScreen = ({ navigation }) => {
         fontWeight: "bold",
         marginBottom: 16,
       },
+      searchInput: {
+        backgroundColor: "#ffffff",
+        borderRadius: 10,
+        padding: 10,
+        fontSize: 16,
+        marginBottom: 10,
+        color: "#000",
+      },
+      pickerContainer: {
+        backgroundColor: "#ffffff",
+        borderRadius: 10,
+        width: 242,
+        paddingHorizontal: 24,
+        alignSelf: "center",
+        marginBottom: 20,
+      },
+      cardContainer: {
+        width: "100%",
+      },
       button: {
         fontSize: 16,
         color: "#007BFF",
         marginBottom: 16,
       },
+      row: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "center",
+      },
+
     });
 
 export default HomeScreen;
