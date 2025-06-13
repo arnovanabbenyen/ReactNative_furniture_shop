bba04460dcfe9f14b8cf2c91c8ea6805f1181cec
Arno Van Abbenyen <r0893222@student.thomasmore.be>
2025-06-13 21:01:06 +0200

    Update: categorieën toegevoegd + styling

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -2,12 +2,14 @@ import React from "react";
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-const ProductCard = ({ image, title, description, price, onPress }) => {
+const ProductCard = ({ image, title, description, price, category, onPress }) => {
   const navigation = useNavigation();
+
   return (
     <View style={styles.card}>
       <Image source={image} style={styles.image} />
       <Text style={styles.title}>{title}</Text>
+      <Text style={styles.category}>{category}</Text>
       <Text style={styles.description}>{description}</Text>
       <Text style={styles.price}>Vanaf €{price}</Text>
       <TouchableOpacity onPress={onPress}>
@@ -36,16 +38,31 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginTop: 8,
   },
+  category: {
+    fontSize: 14,
+    color: "#888",
+    marginTop: 2,
+    marginBottom: 2,
+  },
   description: {
     fontSize: 14,
     color: "#555",
     marginTop: 4,
   },
-    price: {
-        fontSize: 14,
-        color:"#555",
-        marginTop: 38,
-    },
+  price: {
+    fontSize: 14,
+    color: "#555",
+    marginBottom: 8,
+  },
+  button: {
+    fontSize: 16,
+    color: "#ffffff",
+    backgroundColor: "#000000",
+    padding: 8,
+    borderRadius: 20,
+    marginTop: 8,
+    textAlign: "center",
+  },
 });
 
 export default ProductCard;
