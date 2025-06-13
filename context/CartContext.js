b0016d65a81abae2f6f30f0cbdb4ec5f6f7b2c65
Arno Van Abbenyen <r0893222@student.thomasmore.be>
2025-06-13 21:02:10 +0200

    Add: CartContext gemaakt met functies voor het toevoegen, verwijderen en wissen van winkelwagens.

---
+++ context/CartContext.js
@@ -0,0 +1,36 @@
+import React, { createContext, useState } from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prevCart) => {
+      const existing = prevCart.find((item) => item.title === product.title);
+      if (existing) {
+        return prevCart.map((item) =>
+          item.title === product.title
+            ? { ...item, quantity: item.quantity + product.quantity }
+            : item
+        );
+      } else {
+        return [...prevCart, product];
+      }
+    });
+  };
+
+  const removeFromCart = (productId) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
