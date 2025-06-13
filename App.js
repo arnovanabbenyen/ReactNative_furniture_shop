cf8f1f41e27caaa875eb8f75216f9c814b8c620d
Arno Van Abbenyen <r0893222@student.thomasmore.be>
2025-06-13 21:13:18 +0200

    Update: nog extra navigaties toegevoegd + alles ingepakte in CartProvider voor de globale winkelwagenstatus overal te behouden

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
-import React from "react";
-import {NavigationContainer} from "@react-navigation/native";
-import {createStackNavigator} from "@react-navigation/stack";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./screens/HomeScreen.js";
-import ProductDetail from "./screens/ProductDetail.js";
+import Products from "./screens/Products.js";
+import Details from "./screens/ProductDetail.js";
+import Blogs from "./screens/Blogs.js";
+import BlogDetail from "./screens/BlogDetail.js";
+import CustomHeader from "./components/CustomHeader.js";
+import { CartProvider } from "./context/CartContext";
+import CartScreen from "./screens/CartScreen.js";
+import AboutHenk from "./screens/AboutHenkScreen.js";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={ProductDetail} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <CartProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={({ navigation }) => ({
+            header: () => <CustomHeader navigation={navigation} />,
+          })}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Products" component={Products} />
+          <Stack.Screen name="Details" component={Details} />
+          <Stack.Screen name="Blog" component={Blogs} />
+          <Stack.Screen name="BlogDetail" component={BlogDetail} />
+          <Stack.Screen name="Cart" component={CartScreen} />
+          <Stack.Screen name="AboutHenk" component={AboutHenk} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
   );
-} 
+}
